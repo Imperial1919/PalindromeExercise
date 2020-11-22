0b2dd6faf8c5a899cbc4babb8de5e52134adc84a
Bryce <ImperialEagle1998@gmail.com>
2020-11-21 22:31:39 -0600

    Completed with.. a little help.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+
+
+            //word arguement is converted to lowercase lettering
+            var lower = word.ToLower();
+            //word argument after lower letter conversion, is then converted again to a CharArray data type
+            var chararray = word.ToCharArray();
+            //This simply reverses forementioned array
+            var test = chararray.Reverse();
+
+
+            //This assigns the operation of testing for similarities between the fully converted test variable, and the basic low case lettered 'lower' variable
+            bool IsPalindrome = lower.SequenceEqual(test);
+            return IsPalindrome;
+          
+
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
+//never forget the tiny little, 'using' attributes. :)
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("anna",true)]
+        public void WordSmith(string word, bool conditional)
         {
+            // Arrangement Phase:
+            var arrange = new WordSmith();
+
+            //Action Phase:
+            var actual = arrange.IsAPalindrome(word);
+
+            //Assert Phase:
+            Assert.Equal(actual,conditional);
+
+          
 
         }
     }
